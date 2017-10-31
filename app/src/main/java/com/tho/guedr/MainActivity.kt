8a9a323785290dacd31015c70ce3231bc1208461
Carlos <thehandyowl.io@gmail.com>
2017-11-01 00:05:41 +0100

    Mostramos las propiedades, pero falla el humidity

--- app/src/main/java/com/tho/guedr/MainActivity.kt
+++ app/src/main/java/com/tho/guedr/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.tho.guedr
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
+import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +19,24 @@ class MainActivity : AppCompatActivity() {
         
     }
 
-    private fun setForecast(forecast: Forecast) {}
+    private fun setForecast(forecast: Forecast) {
+        // Accedemos a las vistas de la interfaz
+        val forecastImage = findViewById<ImageView>(R.id.forecast_image)
+        val maxTemp = findViewById<TextView>(R.id.max_temp)
+        val minTemp = findViewById<TextView>(R.id.min_temp)
+        val humidity = findViewById<TextView>(R.id.humidity)
+        val forecastDescription = findViewById<TextView>(R.id.forecast_description)
+
+        // Actualizamos la vista con el modelo
+        forecastImage.setImageResource(forecast.icon)
+        forecastDescription.text(forecast.description)
+        val maxTempString = getString(R.string.max_temp_format, forecast.maxTemp)
+        val minTempString = getString(R.string.min_temp_format, forecast.minTemp)
+        val humidityString = getString(R.string.humidity, forecast.humidity)
+        maxTemp.setText(maxTempString)
+        minTemp.setText(minTempString)
+        humidity.setText(humidityString)
+
+    }
 
 }
