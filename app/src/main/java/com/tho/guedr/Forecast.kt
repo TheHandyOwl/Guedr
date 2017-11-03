3ba867ac2c559f29c537300e2786f85e3a43691b
Carlos <thehandyowl.io@gmail.com>
2017-11-03 22:57:00 +0100

    Limitando valores en el constructor del modelo

--- app/src/main/java/com/tho/guedr/Forecast.kt
+++ app/src/main/java/com/tho/guedr/Forecast.kt
@@ -8,6 +8,15 @@ data class Forecast(val maxTemp: Float, val minTemp: Float, val humidity: Float,
 
     protected fun toFahrenheit(celsius: Float) = celsius * 1.8f + 32
 
+    init {
+//        if (humidity < 0 || humidity > 100) {
+//            throw IllegalArgumentException("Humidity should be between 0 and 100")
+//        }
+        if (humidity !in 0f..100f) {
+            throw IllegalArgumentException("Humidity should be between 0 and 100")
+        }
+    }
+
     fun getMaxTemp(units: TempUnit) = when (units) {
         TempUnit.CELSIUS -> maxTemp
         TempUnit.FAHRENHEIT -> toFahrenheit(maxTemp)
