34c54c5610f58a23559e093791c89c454da5e266
Carlos <thehandyowl.io@gmail.com>
2017-11-06 22:58:03 +0100

    Fin parte 1 a medias

--- app/src/main/java/com/tho/guedr/activity/CityPagerActivity.kt
+++ app/src/main/java/com/tho/guedr/activity/CityPagerActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.support.v13.app.FragmentPagerAdapter
 
 import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
 import com.tho.guedr.R
 import com.tho.guedr.fragment.ForecastFragment
 import com.tho.guedr.model.Cities
@@ -17,12 +18,20 @@ class CityPagerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_pager)
 
+        // Configuramos la Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        // Icono de la app
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        setSupportActionBar(toolbar)
+
         val pager = findViewById<ViewPager>(R.id.view_pager)
         val cities = Cities()
         val adapter = object: FragmentPagerAdapter(fragmentManager) {
             override fun getItem(position: Int): Fragment = ForecastFragment.newInstance(cities[position])
 
             override fun getCount(): Int = cities.count
+
+            override fun getPageTitle(position: Int)= cities[position].name
 
         }
 
